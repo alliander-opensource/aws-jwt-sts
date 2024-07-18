044b444928ffad5fbdcbe416ea3870e78b4962d1
Jehannes Zuidema <jkzuidema@outlook.com>
2024-07-18 11:17:08 +0200

    add the build

--- src/index.ts
+++ src/index.ts
@@ -224,9 +224,9 @@ export class AwsJwtSts extends Construct {
       environment: {
         S3_BUCKET: oidcbucket.bucketName,
         ISSUER: issuer,
-        CURRENT_KEY: ('alias/' + (props.currentKeyName ?? 'sts/CURRENT')),
-        PREVIOUS_KEY: ('alias/' + (props.previousKeyName ?? 'sts/PREVIOUS')),
-        PENDING_KEY: ('alias/' + (props.pendingKeyName ?? 'sts/PENDING'))
+        CURRENT_KEY: 'alias/' + (props.currentKeyName ?? 'sts/CURRENT'),
+        PREVIOUS_KEY: 'alias/' + (props.previousKeyName ?? 'sts/PREVIOUS'),
+        PENDING_KEY: 'alias/' + (props.pendingKeyName ?? 'sts/PENDING')
       }
     })
 
