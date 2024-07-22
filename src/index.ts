c5b54e8dd4c7f490a79e9e20f90ae2d2dd055ec3
Jehannes <Jehannes.ZUidema@alliander.com>
2024-07-22 07:28:42 +0200

    add 'alias/' to key name
    Signed-off-by: Jehannes <Jehannes.ZUidema@alliander.com>

--- src/index.ts
+++ src/index.ts
@@ -224,9 +224,9 @@ export class AwsJwtSts extends Construct {
       environment: {
         S3_BUCKET: oidcbucket.bucketName,
         ISSUER: issuer,
-        CURRENT_KEY: props.currentKeyName ?? 'sts/CURRENT',
-        PREVIOUS_KEY: props.previousKeyName ?? 'sts/PREVIOUS',
-        PENDING_KEY: props.pendingKeyName ?? 'sts/PENDING'
+        CURRENT_KEY: 'alias/' + (props.currentKeyName ?? 'sts/CURRENT'),
+        PREVIOUS_KEY: 'alias/' + (props.previousKeyName ?? 'sts/PREVIOUS'),
+        PENDING_KEY: 'alias/' + (props.pendingKeyName ?? 'sts/PENDING')
       }
     })
 
@@ -242,7 +242,7 @@ export class AwsJwtSts extends Construct {
       environment: {
         ISSUER: issuer,
         DEFAULT_AUDIENCE: props.defaultAudience,
-        CURRENT_KEY: props.currentKeyName ?? 'sts/CURRENT'
+        CURRENT_KEY: 'alias/' + (props.currentKeyName ?? 'sts/CURRENT')
       }
     })
 
